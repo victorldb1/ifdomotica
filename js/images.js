6a637877240f5c051efabe18bff698c26b12a313
victorldb1 <vicldb99@gmail.com>
2018-03-03 21:36:57 -0300

    envio

---
+++ js/images.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+	//imagem 1
+  	$("#btn_lamp1").click(function(){
+    	if($("#state_lamp1").attr("src") == "images/luz_apagada.png"){
+    		$("#state_lamp1").attr("src","images/luz_acesa.png");
+    		$("#btn_lamp1").html("Apagar");
+    	}
+    	else {
+    		$("#state_lamp1").attr("src","images/luz_apagada.png");
+    		$("#btn_lamp1").html("Acender");
+    	}
+    });
+  	//imagem 2
+  	$("#btn_lamp2").click(function(){
+    	if($("#state_lamp2").attr("src") == "images/luz_apagada.png"){
+    		$("#state_lamp2").attr("src","images/luz_acesa.png");
+    		$("#btn_lamp2").html("Apagar");
+    	}
+    	else {
+    		$("#state_lamp2").attr("src","images/luz_apagada.png");
+    		$("#btn_lamp2").html("Acender");
+    	}
+    });
+  	//imagem 3
+  	$("#btn_lamp3").click(function(){
+    	if($("#state_lamp3").attr("src") == "images/luz_apagada.png"){
+    		$("#state_lamp3").attr("src","images/luz_acesa.png");
+    		$("#btn_lamp3").html("Apagar");
+    	}
+    	else {
+    		$("#state_lamp3").attr("src","images/luz_apagada.png");
+    		$("#btn_lamp3").html("Acender");
+    	}
+    });
+    //imagem 4
+  	$("#btn_lamp4").click(function(){
+    	if($("#state_lamp4").attr("src") == "images/luz_apagada.png"){
+    		$("#state_lamp4").attr("src","images/luz_acesa.png");
+    		$("#btn_lamp4").html("Apagar");
+    	}
+    	else {
+    		$("#state_lamp4").attr("src","images/luz_apagada.png");
+    		$("#btn_lamp4").html("Acender");
+    	}
+    });
+    //imagem 5
+  	$("#btn_lamp5").click(function(){
+    	if($("#state_lamp5").attr("src") == "images/luz_apagada.png"){
+    		$("#state_lamp5").attr("src","images/luz_acesa.png");
+    		$("#btn_lamp5").html("Apagar");
+    	}
+    	else {
+    		$("#state_lamp5").attr("src","images/luz_apagada.png");
+    		$("#btn_lamp5").html("Acender");
+    	}
+    });
+    //imagem 6
+  	$("#btn_lamp6").click(function(){
+    	if($("#state_lamp6").attr("src") == "images/luz_apagada.png"){
+    		$("#state_lamp6").attr("src","images/luz_acesa.png");
+    		$("#btn_lamp6").html("Apagar");
+    	}
+    	else {
+    		$("#state_lamp6").attr("src","images/luz_apagada.png");
+    		$("#btn_lamp6").html("Acender");
+    	}
+    });
+    //imagem 7
+  	$("#btn_lamp7").click(function(){
+    	if($("#state_lamp7").attr("src") == "images/luz_apagada.png"){
+    		$("#state_lamp7").attr("src","images/luz_acesa.png");
+    		$("#btn_lamp7").html("Apagar");
+    	}
+    	else {
+    		$("#state_lamp7").attr("src","images/luz_apagada.png");
+    		$("#btn_lamp7").html("Acender");
+    	}
+    });
+    //imagem 8
+  	$("#btn_lamp8").click(function(){
+    	if($("#state_lamp8").attr("src") == "images/luz_apagada.png"){
+    		$("#state_lamp8").attr("src","images/luz_acesa.png");
+    		$("#btn_lamp8").html("Apagar");
+    	}
+    	else {
+    		$("#state_lamp8").attr("src","images/luz_apagada.png");
+    		$("#btn_lamp8").html("Acender");
+    	}
+    });
+});
